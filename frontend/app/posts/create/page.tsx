43f69e352fd4485a9eed19c3c9073d77368fb3f5
Dima <dima2019mega@gmail.com>
2024-06-19 16:52:44 +0200

    hot fix

--- frontend/app/posts/create/page.tsx
+++ frontend/app/posts/create/page.tsx
@@ -1,36 +1,37 @@
-import { createSchema, CreateSchema } from '@/lib/types'
-import { useMutation } from '@tanstack/react-query'
+'use client'
+
+import { createSchema, CreateSchema } from '@/lib/types';
+import { useMutation } from '@tanstack/react-query';
 import { zodResolver } from "@hookform/resolvers/zod";
-import axios from 'axios'
-import { useRouter } from 'next/router'
-import { useForm } from 'react-hook-form'
-import { toast } from 'sonner'
+import axios from 'axios';
+// import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 
-export default function createPost() {
-
-  const router = useRouter()
+export default function CreatePost() {
+  // const router = useRouter();
 
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<CreateSchema>({
     resolver: zodResolver(createSchema)
-  })
+  });
 
   const mutation = useMutation({
-    mutationFn: (newToDo:CreateSchema) => {
-      return axios.post('http://localhost:5000/posts', newToDo)
+    mutationFn: (newToDo: CreateSchema) => {
+      return axios.post('http://localhost:5000/posts', newToDo);
     },
     onSuccess: () => {
-      toast.success('Zadanie zostało stworzone')
-      router.push('/posts')
+      toast.success('Zadanie zostało stworzone');
+      // router.push('/');
     },
     onError: (error) => {
-      toast.error("Coś poszło nie tak")
-      console.log(error)
+      toast.error("Coś poszło nie tak");
+      console.log(error);
     }
-  })
+  });
 
-  const onSubmit = (data:CreateSchema) => {
-    mutation.mutate({...data})
-  }
+  const onSubmit = (data: CreateSchema) => {
+    mutation.mutate({ ...data });
+  };
 
   return (
     <div>
@@ -38,7 +39,8 @@ export default function createPost() {
         <input type="text" {...register("title")} />
         <input type="text" {...register("description")} />
         <input type="text" {...register("author")} />
+        <button type="submit" disabled={isSubmitting}>Create Post</button>
       </form>
     </div>
-  )
+  );
 }
