97eb80c9eba74f24b5c47e0f30fae9b392bf5c1f
Dima <dima2019mega@gmail.com>
2024-06-23 15:35:56 +0200

    adding tasks to my calendar

--- frontend/components/Calendar.tsx
+++ frontend/components/Calendar.tsx
@@ -1,67 +1,62 @@
+"use client"
+
 import FullCalendar from '@fullcalendar/react';
 import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { Post } from '@/lib/types';
 
 export default function ResourceCalendar() {
-  const handleDateClick = (arg:any) => {
+
+  const { isLoading, data, error } = useQuery({
+    queryKey: ['todo'],
+    queryFn: async () => {
+      const response = await axios.get<Post[]>('http://localhost:5000/posts')
+      return response.data
+    }
+  })
+  
+  const handleDateClick = (arg) => {
     alert(arg.dateStr);
   };
 
-  const renderEventContent = (eventInfo:any) => {
+  const renderEventContent = (eventInfo) => {
     return (
-      <div className='flex flex-col justify-between py-3    px-4 h-full'>
+      <div className='flex flex-col h-full justify-between px-4 py-2'>
         <div>
-          <h1 className=' font-bold text-xl'>{eventInfo.event.title}</h1>
-          <p>{eventInfo.event.extendedProps.description}</p>
+          <p className=' font-bold text-xl '>{eventInfo.event.title}</p>
+          <p className=' font-medium text-md'>{eventInfo.event.extendedProps.description}</p>
         </div>
         <b>{eventInfo.timeText}</b>
       </div>
     );
   };
 
+  const events = (data || []).map((post: Post) => ({
+    id: post._id.toString(), 
+    resourceId: 'a', 
+    title: post.title.toString(), 
+    start: post.start_time, 
+    end: post.end_time,
+    extendedProps: {
+      description: post.description
+    }
+  }))
+
   return (
     <div className='max-h-[80%]'>
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin, resourceTimeGridPlugin]}
+        initialView='resourceTimeGridWeek'
         dateClick={handleDateClick}
         eventContent={renderEventContent}
         resources={[
           { id: 'a', title: 'Resource A' },
         ]}
-        events={[
-          { 
-            id: '1', 
-            resourceId: 'a', 
-            title: 'Event 1', 
-            start: '2024-06-22T10:00:00', 
-            end: '2024-06-22T16:00:00',
-            extendedProps: {
-              description: 'Description for Event 1'
-            }
-          },
-          { 
-            id: '2', 
-            resourceId: 'b', 
-            title: 'Event 2', 
-            start: '2023-06-23T13:00:00', 
-            end: '2023-06-23T14:00:00',
-            extendedProps: {
-              description: 'Description for Event 2'
-            }
-          },
-          { 
-            id: '3', 
-            resourceId: 'a', 
-            title: 'Event 3', 
-            start: '2023-06-24T09:00:00', 
-            end: '2023-06-24T10:30:00',
-            extendedProps: {
-              description: 'Description for Event 3'
-            }
-          },
-        ]}
-        resourceAreaWidth='200px'
+        events={events}
+        resourceAreaWidth='300px'
         resourceAreaHeaderContent='Resources'
       />
     </div>
