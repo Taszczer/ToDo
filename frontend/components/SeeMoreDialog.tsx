732fc875db0693b37a3317a2ac3c3622deb15d1d
Dima <dima2019mega@gmail.com>
2024-06-25 21:31:28 +0200

    css for Dialog and small changes for homepage

--- frontend/components/SeeMoreDialog.tsx
+++ frontend/components/SeeMoreDialog.tsx
@@ -18,13 +18,15 @@ export default function SeeMoreDialog({ isOpen, setIsOpen, title, description, t
             leaveTo="opacity-0 scale-95"
           >
             <div className="fixed inset-0 flex items-center justify-center p-4">
-              <Dialog.Panel className="max-w-lg space-y-4 border rounded-xl bg-white min-h-[400px] min-w-[300px] p-12 shadow-lg transform transition-all">
-                <DialogTitle className="font-bold text-2xl">{title}</DialogTitle>
-                <Dialog.Description className="font-medium text-md">{description}</Dialog.Description>
+              <Dialog.Panel className=" space-y-4 rounded-xl bg-[#F9C784] max-w-[300px] flex flex-col justify-between border-[#FCAF58] border-b-8 min-h-[400px] min-w-[300px] p-12 shadow-2xl text-white transform transition-all">
+                <div>
+                  <DialogTitle className="font-bold text-2xl">{title}</DialogTitle>
+                  <Dialog.Description className="font-medium text-md mt-1">{description}</Dialog.Description>
+                </div>
                 <div className='flex justify-between'>
-                  <p>{time}</p>
+                  <p className='font-bold'>{time}</p>
                   <div className="flex gap-4">
-                    <button className="" onClick={() => setIsOpen(false)}>x</button>
+                    <button className="" onClick={() => setIsOpen(false)}>Close</button>
                   </div>
                 </div>
               </Dialog.Panel>
