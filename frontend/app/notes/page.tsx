e5da9feb685bfd1415a4f956f84c6ead115245c5
Dima <dima2019mega@gmail.com>
2024-07-05 14:34:49 +0200

    small changes

--- frontend/app/notes/page.tsx
+++ frontend/app/notes/page.tsx
@@ -27,14 +27,16 @@ export default function NoteBook() {
         <div className=" max-w-[500px]">
             <div className="mt-3 border-4 rounded-3xl border-blue-600 px-5 py-3 flex flex-col">
                 <h1 className="text-xl font-bold text-blue-400">Your notes</h1>
-                {data?.map((note) => (
-                    <Link href={`/notes/${note._id}`}>
-                        <div key={note._id} className="mt-3 border-2 border-blue-400 p-2 px-4 rounded-2xl">
-                            {/* <img src="" alt="" /> */}
-                            <h1 className="font-bold text-blue-500 capitalize">{note.title}</h1>
-                        </div>
-                    </Link>
-                ))}
+                <div className="max-h-[400px] overflow-y-auto">
+                    {data?.map((note) => (
+                        <Link href={`/notes/${note._id}`}>
+                            <div key={note._id} className="mt-3 border-2 border-blue-400 p-2 px-4 rounded-2xl">
+                                {/* <img src="" alt="" /> */}
+                                <h1 className="font-bold text-blue-500 capitalize">{note.title}</h1>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
                 <Link href={'/notes/createNote'} className="mt-2 flex items-center justify-center"><p className=" border-blue-600 border-r-4 border-b-4 bg-blue-400 text-white w-[50px] h-[50px] rounded-full font-bold text-xl flex justify-center items-center">+</p></Link>
             </div>
         </div>
