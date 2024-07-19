0de9baae4cc283c1e1465fa5a3d011f592431629
Dima <dima2019mega@gmail.com>
2024-07-19 18:02:17 +0200

    small changes

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useUser } from './providers/AuthProvider'
 
 export default function Navbar() {
+  const user = useUser()
   
-  return (
-    <div className='absolute right-52 flex justify-end items-center'>
-      <div className='flex gap-24 '>
-          <button className='font-bold text-xl bg-blue-300 text-white px-6 py-2 rounded-md'>Log in</button>
-          <button className='font-bold text-xl bg-orange-300 text-white px-6 py-2 rounded-md'>Sign in</button>
+  if (!user) {
+    return (
+      <div className='absolute right-52 flex justify-end items-center'>
+        <div className='flex gap-24 '>
+            <button className='font-bold text-xl bg-blue-300 text-white px-6 py-2 rounded-md'>Log in</button>
+            <button className='font-bold text-xl bg-orange-300 text-white px-6 py-2 rounded-md'>Sign in</button>
+        </div>
       </div>
-    </div>
-  )
+      )
+  } else {
+    <p>It's workkkkkkk</p>
+  }
 }
