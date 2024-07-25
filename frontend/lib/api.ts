eab1fc91d8a975e4f91eb4ee0546a58999009a26
Dima <dima2019mega@gmail.com>
2024-07-25 21:42:40 +0200

    uncommited some code

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -15,27 +15,27 @@ export async function deleteNote(id: any) {
     await axios.delete(`http://localhost:5000/notes/delete/${id}`)
 }
 
-// export async function login(body: CreateLoginSchema): Promise<User | null> {
-//     await axios.post("http://localhost:5000/login", body, { withCredentials: true });
-//     return await whoAmI();
-// }
+export async function login(body: CreateLoginSchema): Promise<User | null> {
+    await axios.post("http://localhost:5000/login", body, { withCredentials: true });
+    return await whoAmI();
+}
 
-// export async function singIn(body:CreateSigninSchema): Promise<User | null> {
-//     return await axios.post("http://localhost:5000/register", body, {
-//         // withCredentials:true,
-//         headers: { "Content-Type": "multipart/form-data" },
-//     })
-// }
+export async function singIn(body:CreateSigninSchema): Promise<User | null> {
+    return await axios.post("http://localhost:5000/register", body, {
+        // withCredentials:true,
+        headers: { "Content-Type": "multipart/form-data" },
+    })
+}
 
-// export async function whoAmI(): Promise<User | null> {
-//     try {
-//       return (await axios.get<User>("http://localhost:5000/whoami", { withCredentials: true })).data;
-//     } catch (error) {
-//       console.log(error)
-//       return null
-//     }
-// }
+export async function whoAmI(): Promise<User | null> {
+    try {
+      return (await axios.get<User>("http://localhost:5000/whoami", { withCredentials: true })).data;
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+}
   
-// export async function resumeSession(): Promise<User | null> {
-//     return await whoAmI();
-// }
+export async function resumeSession(): Promise<User | null> {
+    return await whoAmI();
+}
