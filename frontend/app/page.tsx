5a7ab7b7f423fb58b415abe2e27868c60733a241
Dima <dima2019mega@gmail.com>
2024-09-12 20:00:05 +0200

    change some css

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -30,14 +30,18 @@ export default function Home() {
     <main className='h-full'>
       <div className="max-w-[100%]">
         <Navbar />
-        <div className="mt-12 ml-10 max-w-[70%]">
-          <Calendar />
-        </div>
-        <div className="absolute top-[60%] 2xl:top-[90%] right-32 border-4 border-orange-secondary py-6 px-2 rounded-xl">
-          <CreatePost />
-        </div>
-        <div className="absolute xl:top-32 xl:right-32 2xl:left-32 2xl:top-[90%] min-w-[500px]">
-          <NoteBook />
+        <div className='flex justify-between'>
+          <div className="mt-12 ml-10 ">
+            <Calendar />
+          </div>
+          <div className='flex flex-col justify-between items-end mr-36'>
+            <div className="max-w-[375px] max-h-[425px] border-4 border-orange-secondary py-6 px-2 rounded-xl mt-28  ">
+              <CreatePost />
+            </div>
+            <div className=" min-w-[500px]">
+              <NoteBook />
+            </div>
+          </div>
         </div>
       </div>
     </main>
